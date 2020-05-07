225d0816a37356f4018497dd6eccdfa8fe25eae3
Mohnish Goyal <57796631+mohnishgoyal@users.noreply.github.com>
2020-05-07 22:40:15 +0530

    Add files via upload

---
+++ Internship-coding_gurus/script.js
@@ -0,0 +1,37 @@
+// set up text to print, each item in array is new line
+var aText = new Array(
+"Started by a group of IITians and NITians to help school children to learn to think, stimulate their creativity and develop their cognitive abilities, DreamAdvance provides practical knowledge and training to the students helping them get familiarized with the concepts learned from books. We have already conducted more than 30 workshops across different cities and have stimulated more than 15,000 young minds."
+);
+var iSpeed = 30; // time delay of print out
+var iIndex = 0; // start printing array at this posision
+var iArrLength = aText[0].length; // the length of the text array
+var iScrollAt = 20; // start scrolling up at this many lines
+ 
+var iTextPos = 0; // initialise text position
+var sContents = ''; // initialise contents variable
+var iRow; // initialise current row
+ 
+function typewriter()
+{
+ sContents =  ' ';
+ iRow = Math.max(0, iIndex-iScrollAt);
+ var destination = document.getElementById("typedtext");
+ 
+ while ( iRow < iIndex ) {
+  sContents += aText[iRow++] + '<br />';
+ }
+ destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
+ if ( iTextPos++ == iArrLength ) {
+  iTextPos = 0;
+  iIndex++;
+  if ( iIndex != aText.length ) {
+   iArrLength = aText[iIndex].length;
+   setTimeout("typewriter()", 500);
+  }
+ } else {
+  setTimeout("typewriter()", iSpeed);
+ }
+}
+
+
+typewriter();
